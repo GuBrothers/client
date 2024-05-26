32cc4bc02d84b5db272d44cb24da198286c9794a
Leebonggu <leebonggu12@hanmail.net>
2024-05-26 23:38:02 +0900

    add nav2

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -3,16 +3,26 @@
 import Link from 'next/link';
 import React from 'react';
 
+const routes = [
+  { name: 'About', href: '/about' },
+  {
+    name: 'Blog',
+    href: 'https://blog.naver.com/mingu_admattorney',
+    target: '_blank',
+  },
+];
+
 function Nav() {
   return (
     <nav className="w-screen h-20 flex justify-center items-center">
       <div className="w-full h-full max-w-6xl px-4 flex justify-between items-center">
         <div>logo</div>
         <div className="flex gap-2">
-          <Link href="/about">About</Link>
-          <Link href="https://blog.naver.com/mingu_admattorney" target="_blank">
-            Blog
-          </Link>
+          {routes.map((route) => (
+            <Link key={route.name} href={route.href} target={route?.target}>
+              {route.name}
+            </Link>
+          ))}
         </div>
       </div>
     </nav>
